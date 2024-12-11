21c50448a08e09d232a12914fb9b0969b9f0ea5d
binwoonki <vinwoonki98@gmail.com>
2024-12-11 14:00:38 +0900

    회원가입 UI작업

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,30 +1,112 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
 
-const Form = styled.form``;
+const Title = styled.h1`
+  font-size: 42px;
+`;
 
-const Input = styled.input``;
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
 
 export default function CreateAccount() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setIsError] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Wrapper>
-      <Form>
-        <Input name="name" placeholder="Name" type="text" required />
-        <Input name="email" placeholder="Email" type="email" required />
+      <Title>Join ❌</Title>
+      <Form onSubmit={handleSubmin}>
         <Input
+          onChange={handleChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required
+        />
+        <Input
+          onChange={handleChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={handleChange}
           name="password"
+          value={password}
           placeholder="Password"
           type="password"
           required
         />
         <Input
-          name="submit"
-          placeholder="Create Account"
-          type="text"
-          required
+          value={isLoading ? "Loading..." : "Create Account"}
+          type="submit"
         />
       </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
     </Wrapper>
   );
 }
