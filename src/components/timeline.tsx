4bf864134fc8506c6591399de59d9a7bc7eeae3f
binwoonki <vinwoonki98@gmail.com>
2024-12-17 17:59:53 +0900

    Realtime Tweets : 실시간으로 데이터를 받아오기, 25개씩 제한, 구독취소 기능 구현

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,16 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  // getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../routes/firebase";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export interface Itweet {
   id: string;
@@ -13,26 +21,43 @@ export interface Itweet {
   createAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<Itweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createAt", "desc")
-    );
-
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, createAt, userId, username, photo } = doc.data();
-      return { tweet, createAt, userId, username, photo, id: doc.id };
-    });
-    setTweets(tweets);
-  };
 
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createAt", "desc"),
+        limit(25)
+      );
+
+      // const snapshot = await getDocs(tweetsQuery);
+      // const tweets = snapshot.docs.map((doc) => {
+      //   const { tweet, createAt, userId, username, photo } = doc.data();
+      //   return { tweet, createAt, userId, username, photo, id: doc.id };
+      // });
+
+      unsubscribe = onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createAt, userId, username, photo } = doc.data();
+          return { tweet, createAt, userId, username, photo, id: doc.id };
+        });
+        setTweets(tweets);
+      });
+    };
     fetchTweets();
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
