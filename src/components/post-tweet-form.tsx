04eccb349bc902375d5996bbd5607e92fe6314ac
binwoonki <vinwoonki98@gmail.com>
2024-12-17 15:37:45 +0900

    Tweeting to Firestore : 파이어베이스 게시물, 이미지 연결하기

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth, db } from "../routes/firebase";
 
 const Form = styled.form`
   display: flex;
@@ -68,8 +70,29 @@ export default function PostTweetForm() {
       setFile(files[0]);
     }
   };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+
+    try {
+      setIsLoading(true);
+      await addDoc(collection(db, "tweets"), {
+        tweet,
+        createAt: Date.now(),
+        username: user.displayName || "Anoymous",
+        userId: user.uid,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <TextArea
         rows={5}
         maxLength={180}
